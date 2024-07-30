1ffe9b28989aaf969de59329c1ae0f91d0dfe75e
Spajro <rafal.spyra@onet.pl>
2024-07-30 17:28:06 +0200

    add temporary example of graphics usage in main

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,63 @@
 use std::thread::sleep;
 use std::time::Duration;
+use minifb::{Key, Window, WindowOptions};
 use crate::engine::Engine;
+use crate::graphics::buffer::{Buffer, Paintable};
+use crate::graphics::point::Point;
+use crate::graphics::triangle::Triangle;
+use crate::graphics::vector::Vector;
 use crate::status::Status;
 use crate::vector2d::Vector2D;
-
 
 mod status;
 mod engine;
 mod vector2d;
 mod normalized2d;
 
+mod graphics;
+
 
 fn main() {
-    let mut engine=Engine::new();
-    let mut status =Status::new();
-    status.accelerate(Vector2D::new(100,0));
-    engine.register(status);
-    for _ in 0..100{
-        engine.update();
+    const WIDTH: usize = 640;
+    const HEIGHT: usize = 360;
+    let mut engine = Engine::new();
+    let mut window = Window::new(
+        "Test - ESC to exit",
+        WIDTH,
+        HEIGHT,
+        WindowOptions::default(),
+    ).unwrap_or_else(|e| { panic!("{}", e); });
+
+    window.set_target_fps(60);
+
+    let mut status1 = Status::new();
+    status1.accelerate(Vector2D::new(5, 0));
+    status1.position.x = 100;
+    status1.position.y = 100;
+    engine.register(status1);
+    let mut status2 = Status::new();
+    status2.accelerate(Vector2D::new(0, 5));
+    status2.position.x = 200;
+    status2.position.y = 200;
+    engine.register(status2);
+
+    while window.is_open() && !window.is_key_down(Key::Escape) {
+        println!("|{}|", engine);
         sleep(Duration::from_secs(1));
-        println!("|{}|", engine)
+
+        let mut buffer = Buffer::new(WIDTH, HEIGHT);
+        engine.update();
+
+
+        engine.objects.iter()
+            .map(|status| Triangle::equilateral(
+                Point::new(status.position.x.clone() as usize, status.position.y.clone() as usize),
+                Vector::new(status.speed.x.clone() as i32, status.speed.y.clone() as i32).normalize(),
+                50))
+            .for_each(|triangle|triangle.paint(&mut buffer));
+
+        window
+            .update_with_buffer(&buffer.buffer, WIDTH, HEIGHT)
+            .unwrap();
     }
 }
